6de5d6d161c8262a8a9f841501f6f8cd7fce489b
manta726 <sabenihfermanta@gmail.com>
2025-12-30 15:11:56 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
   images: {
     unoptimized: true,
   },
- 
-}
+};
 
-export default nextConfig
+export default nextConfig;
