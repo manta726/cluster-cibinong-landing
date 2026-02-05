1466622f2df5f0193da041a2ea0125b600aef183
manta726 <sabenihfermanta@gmail.com>
2026-02-05 15:55:36 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',  // ⬅️ TAMBAHKAN INI!
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
- 
+  trailingSlash: true,  // ⬅️ Recommended untuk Firebase Hosting
 }
 
 export default nextConfig
